a2bb5e344a42d0b18fd588e8644f73059355147d
Aserrano7 <serranoandres022@gmail.com>
2023-10-30 13:17:02 -0700

    added the questions array

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        title: "The condition of an if/else statement is enclosed within ______.",
+        options: [
+            { choice: 'Quotes', check: false },
+            { choice: 'Curly Brackets', check: true },
+            { choice: 'Parentheses', check: false },
+            { choice: 'Square Brackets', check: false }
+        ]
+    },
+    {
+        title: "Commonly used data types do NOT include:",
+        options: [
+            { choice: 'strings', check: false },
+            { choice: 'booleans', check: false },
+            { choice: 'alerts', check: true },
+            { choice: 'number', check: false }
+        ]
+    },
+    {
+        title: "Arrays in Javascript can be used to store ______.",
+        options: [
+            { choice: 'Number and Strings', check: false },
+            { choice: 'Other Arrays', check: false },
+            { choice: 'Booleans', check: false },
+            { choice: 'All of the Above', check: true }
+        ]
+    },
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables.",
+        options: [
+            { choice: 'Quotes', check: true },
+            { choice: 'Curly Brackets', check: false },
+            { choice: 'Commas', check: false },
+            { choice: 'Parenthese', check: false }
+        ]
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            { choice: 'Javascript', check: false },
+            { choice: 'console.log', check: true },
+            { choice: 'Terminal/Bash', check: false },
+            { choice: 'For Loops', check: false }
+        ]
+    },
+];
