19cd2f8bb74da942894ff8635e4ea3e552e6ab02
Aserrano7 <serranoandres022@gmail.com>
2023-10-30 13:35:08 -0700

    added css, displayQuestion, reset and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,16 @@ var strBox = document.getElementById('start-box');
 var questionBox = document.getElementById('question-box');
 var allQuestions;
 var questionIndex;
+var questionTitle = document.getElementById('title');
+var answBtn = document.getElementById('answer-buttons');
+var checkAnswer = document.getElementById('checkAnswer');
+var nextBtn = document.getElementById('next-button');
+
+document.getElementById('start-button').addEventListener('click', startGame);
+nextBtn.addEventListener('click', () => {
+    questionIndex++;
+    nextQuestion();
+});
 function startGame() {
     timeStr = setInterval(timer, 1000);
     strBox.classList.add('hide');
@@ -62,9 +72,37 @@ function startGame() {
     questionIndex = 0;
     questionBox.classList.remove('hide');
     timer();
+    nextQuestion();
     
 }
 function timer() {
     timeLeft--;
     timerEl.textContent = "Time: " + timeLeft;
-}
+};
+function nextQuestion() {
+    reset();
+    displayQuestion(allQuestions[questionIndex]);
+};
+function reset() {
+    nextBtn.classList.add("hide");
+    checkAnswer.classList.add("hide");
+    while (answBtn.firstChild) {
+        answBtn.removeChild(answBtn.firstChild);
+    }
+};
+
+
+function displayQuestion(question) {
+    question.options.forEach(selected => {
+        var button = document.createElement('button');
+        button.innerText = selected.choice;
+        button.classList.add('button');
+        if (selected.check) {
+            button.dataset.check = selected.check;
+        }
+        questionTitle.innerText = question.title;
+        answBtn.appendChild(button);
+
+    });
+
+};
