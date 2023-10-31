6ebb7809089f3b9ecc7d72e58df38ac3c9e2d9c0
Aserrano7 <serranoandres022@gmail.com>
2023-10-30 17:37:47 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// This array  will hold all my questions and answers
 var questions = [
     {
         title: "The condition of an if/else statement is enclosed within ______.",
@@ -47,7 +48,7 @@ var questions = [
 ];
 
 
-
+//These are all my declared variables that you can use globaly
 var timerEl = document.getElementById("timer");
 var timeLeft = 75;
 var timeStr;
@@ -65,30 +66,36 @@ var scoreForm = document.getElementById('score-form');
 var initialUser = document.getElementById('initials-box');
 var highscore = document.getElementById('highscores-box');
 
+//listens for start button to be presed and calls startGame funtion
 document.getElementById('start-button').addEventListener('click', startGame);
-
+//listens for next button to be pressed and if so increases question index and call next question
 nextBtn.addEventListener('click', () => {
     questionIndex++;
     nextQuestion();
 });
 
+//listens for viewhighscore to be clicked if so calls display function
 document.getElementById('score-link').addEventListener('click', displayHighscore);
-
+//listens for submit button to be clicked if so calls the displayHighscore function with 
+// the users initials in the parameters
 submit.addEventListener('click', function (event) {
     event.preventDefault();
     var initialUser = document.getElementById('initials-box').value;
     displayHighscore(initialUser);
 });
+
+//listens for restart button to be clicked if so reloads the page
 document.getElementById('restart-quiz').addEventListener('click', function (event) {
     location.reload();
 
 });
-
+//listens for clear button to be called if so clears localstorage and innerHTML
 document.getElementById('clear-button').addEventListener("click", function () {
     localStorage.clear();
     document.getElementById("highscore").innerHTML = "";
 });
-
+//starts the  time and hides everything besides the question box 
+//and calls the nextQuestion function
 function startGame() {
     timeStr = setInterval(timer, 1000);
     strBox.classList.add('hide');
@@ -99,6 +106,8 @@ function startGame() {
     nextQuestion();
 
 }
+//sets our timer to subtract by 1 second and displays it on 
+//header and if time reaches 0 then  display the players score
 function timer() {
     timeLeft--;
     timerEl.textContent = "Time: " + timeLeft;
@@ -106,10 +115,17 @@ function timer() {
         displayScore();
     }
 };
+
+//resets the page and uses different index to call the 
+//displayQuestion function with the the new index 
 function nextQuestion() {
     reset();
     displayQuestion(allQuestions[questionIndex]);
 };
+
+//resets the page so that the next button and the checkAnswer button is hidden using the 
+//hide class, as well as removes all the buttons that were first created by 
+//displayQuestion function
 function reset() {
     nextBtn.classList.add("hide");
     checkAnswer.classList.add("hide");
@@ -118,7 +134,9 @@ function reset() {
     }
 };
 
-
+//creates new buttons for all options from questions array 
+//and makes the container for them and appends them to the
+//page, running them trhough a for.each loop 
 function displayQuestion(question) {
     question.options.forEach(selected => {
         var button = document.createElement('button');
@@ -133,6 +151,12 @@ function displayQuestion(question) {
     });
 
 };
+//checks what choice the user selected and checks if 
+// is wrong or correct, while also subtracting 10 seconds
+// from the timer if  the users chooses the wrong answer
+//once time runs out or player reaches the end of the questions
+//then it calls the function displayScore to show the players 
+//score
 function selectChoice(event) {
     var selectedBtn = event.target;
     var correct = selectedBtn.dataset.check;
@@ -156,7 +180,8 @@ function selectChoice(event) {
         displayScore();
     }
 }
-
+//clears the timer and adds the hide class to everthing besides the score-box
+//as well as displays the users time and their score
 function displayScore() {
     clearInterval(timeStr);
     timerEl.textContent = 'Time: ' + timeLeft;
@@ -167,7 +192,11 @@ function displayScore() {
     playersScore.textContent = 'Your Score is ' + timeLeft;
 }
 
-
+//this function uses the user initials to save their score 
+//and initials to the local storage as well as storing them 
+//in ther own div to display the users score. 
+// it runs a for loop to attach this to every user that plays 
+// the quiz
 function displayHighscore(initialUser) {
     highscore.classList.remove('hide');
     document.getElementById('score-box').classList.add('hide');
